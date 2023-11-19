08762dfb212da54e9f841f0e3f5ac46ceb5437a8
Hedgehog68 <75954520+Hedgehog68@users.noreply.github.com>
2023-11-19 14:02:49 -0500

    docker

--- install-chrome.js
+++
@@ -1,9 +0,0 @@
-import { install } from 'puppeteer-core/lib/install.js';
-
-async function installChrome() {
-  console.log('Installing Chrome...');
-  await install({ product: 'chrome' });
-  console.log('Chrome installation complete.');
-}
-
-installChrome();
