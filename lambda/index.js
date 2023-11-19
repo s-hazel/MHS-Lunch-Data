298eedad2625bd88256b29dd49355a80f5b0d0ec
Hedgehog68 <75954520+Hedgehog68@users.noreply.github.com>
2023-11-19 00:26:03 -0500

    fix

---
+++ lambda/index.js
@@ -0,0 +1,62 @@
+import * as puppeteer from 'puppeteer';
+const { launch } = puppeteer;
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', async (req, res) => {
+    try {
+        const browser = await launch({ headless: "new" });
+
+        const page = await browser.newPage();
+        await page.goto('https://melroseschools.nutrislice.com/menu/melrose/breakfast', { waitUntil: 'networkidle0' });
+
+        const button = await page.$('.primary');
+        if (button) {
+            await button.click();
+        }
+
+        try {
+            // Wait for either .food-name-container or .no-data to appear
+            await page.waitForSelector('.food-name-container, .no-data');
+        } catch (error) {
+            console.log('Timeout waiting for .food-name-container or .no-data');
+            await browser.close();
+            res.send("data: No data available");
+            return; // Add return statement to exit the function
+        }
+
+        // Check if .no-data exists
+        const noDataElement = await page.$('.no-data');
+        if (noDataElement) {
+            console.log('No data available');
+            await browser.close();
+            res.send("data: No lunch today");
+            return; // Add return statement to exit the function
+        }
+
+        // Continue if .no-data is not found
+        const foodNameContainer = await page.$('.food-name-container');
+
+        if (!foodNameContainer) {
+            console.log('Food name not found');
+            await browser.close();
+            res.send("data: No data available");
+            return; // Add return statement to exit the function
+        }
+
+        const textContent = await page.evaluate(el => el.textContent, foodNameContainer);
+
+        await browser.close();
+
+        res.send('data: ' + textContent);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
