b51af67b1c6da037245c76a43de32ec74a06b2b6
Hedgehog68 <75954520+Hedgehog68@users.noreply.github.com>
2023-11-19 14:09:12 -0500

    args

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import * as puppeteer from 'puppeteer';
 const { launch } = puppeteer;
 import express from 'express';
+require("dotenv").config()
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,9 +9,16 @@ const port = process.env.PORT || 3000;
 app.get('/', async (req, res) => {
     try {
         const browser = await puppeteer.launch({
-            executablePath: puppeteer.executablePath(),
-            headless: "new"
-          });
+            args: [
+                "--disable-setuid-sandbox",
+                "--no-sandbox",
+                "--single-process",
+                "--no-zygote"
+            ],
+            executablePath: process.env.NODE_ENV === 'production'
+             ? process.env.PUPPETEER_EXECUTABLE_PATH
+             : puppeteer.executablePath()
+        });
 
         const page = await browser.newPage();
         await page.goto('https://melroseschools.nutrislice.com/menu/melrose/breakfast', { waitUntil: 'networkidle0' });
