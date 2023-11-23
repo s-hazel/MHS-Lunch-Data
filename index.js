f4ec83c0524dc8196e137c9d633e3d4c4755a27e
Hedgehog68 <75954520+Hedgehog68@users.noreply.github.com>
2023-11-23 00:18:03 -0500

    final

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ app.get('/', async (req, res) => {
             args: [
                 "--disable-setuid-sandbox",
                 "--no-sandbox",
-                "--single-process",
                 "--no-zygote"
             ],
             executablePath: process.env.NODE_ENV === 'production'
